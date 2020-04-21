71f9bac316ac3fa977c03bc603621829409f9fb7
ZiQi <1157988746@qq.com>
2020-04-21 20:59:42 +0800

    将px转换为rem

---
+++ postcss.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  plugins: {
+    autoprefixer: {
+      browsers: ['Android >= 4.0', 'iOS >= 8'],
+    },
+    'postcss-pxtorem': {
+      //转换的基准值 1rem = 37.5px
+      rootValue: 37.5,
+      propList: ['*'],
+    },
+  },
+}
