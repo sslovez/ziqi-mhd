a560f3e0bca6f55a0ea7a31018af8d406b0a1c05
ZiQi <1157988746@qq.com>
2020-04-21 21:17:11 +0800

    buffix:autoprefixer的配置处理

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   plugins: {
     autoprefixer: {
-      browsers: ['Android >= 4.0', 'iOS >= 8'],
+      //不需要设置这个选项
+      // browsers: ['Android >= 4.0', 'iOS >= 8'],
     },
     'postcss-pxtorem': {
       //转换的基准值 1rem = 37.5px
